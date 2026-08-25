08f89cc6f0f06d005d56b68cb7f42b178b40d8ae
Nelson Katsala <nelson.katsala@MW.GT.COM>
2026-08-25 21:06:20 +0200

    Describe your changes

--- src/pages/POSPage.tsx
+++ src/pages/POSPage.tsx
@@ -175,7 +175,7 @@ export default function POSPage() {
         </div>
 
         {/* Desktop: Side-by-Side Layout */}
-        <div className="hidden lg:grid grid-cols-[minmax(500px,1.5fr)_24px_minmax(450px,1fr)] h-full gap-0 p-6 max-w-[1920px] mx-auto w-full">
+        <div className="hidden lg:grid grid-cols-[minmax(500px,1.3fr)_24px_minmax(560px,1.2fr)] h-full gap-0 p-6 max-w-[1920px] mx-auto w-full">
           {/* Left Panel: Medicine Selector */}
           <div className="bg-white rounded-2xl border border-primary-100 p-6 overflow-y-auto min-h-0">
             <MedicineSelector medicines={medicines} isLoading={isLoading} />
@@ -185,7 +185,7 @@ export default function POSPage() {
           <div></div>
 
           {/* Right Panel: Cart */}
-          <div className="bg-white rounded-2xl border border-primary-100 p-6 overflow-y-auto min-h-0 flex flex-col">
+          <div className="bg-white rounded-2xl border border-primary-100 p-6 overflow-y-auto min-h-0 flex flex-col h-full">
             <Cart onCheckout={handleCheckout} />
           </div>
         </div>
