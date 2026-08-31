67bbbb405d8c53ee2462462977ae1a36db6e583e
Nelson Katsala <nelson.katsala@MW.GT.COM>
2026-08-31 20:27:05 +0200

    Describe your changes

--- src/components/POS/Receipt.tsx
+++ src/components/POS/Receipt.tsx
@@ -101,15 +101,6 @@ export default function Receipt({ isOpen, data, onClose }: ReceiptProps) {
     })
   }
 
-  const formatTime = (dateString: string) => {
-    const date = new Date(dateString)
-    return date.toLocaleTimeString('en-US', {
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    })
-  }
-
   return (
     <>
       {/* Modal Overlay */}
